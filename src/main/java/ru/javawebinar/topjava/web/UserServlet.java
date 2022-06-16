8e5ab06d078f4eca96b7fff23211968b6a638660
FonarevaOlga <oefonareva@edu.hse.ru>
2022-06-16 18:57:20 +0300

    add MyMealServer

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,11 +12,13 @@ import java.io.IOException;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class UserServlet extends HttpServlet {
-    private static final Logger log = getLogger(UserServlet.class);
+        private static final Logger LOG = LoggerFactory.getLogger(UserServlet.class);
+   // private static final Logger log = getLogger(UserServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.debug("forward to users");
-        request.getRequestDispatcher("/users.jsp").forward(request, response);
+        LOG.debug("redirect to users");
+        response.sendRedirect("users.jsp");
+//        request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
 }
