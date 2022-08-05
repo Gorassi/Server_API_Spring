a4617ac40942e4eb303c15cbed7d56a504e2c398
FonarevaOlga <oefonareva@edu.hse.ru>
2022-08-05 13:48:45 +0300

    4_01_fix_Problems_Meal_Null

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -19,7 +19,7 @@ CREATE TABLE users
 CREATE TABLE user_roles
 (
     user_id INTEGER NOT NULL,
-    role    VARCHAR,
+    role    VARCHAR NOT NULL,
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
